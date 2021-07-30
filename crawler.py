61d2545b737b115cff5a3a3762e935003f6f37b4
lariskovski <larissaporto@live.com>
2021-07-30 18:13:15 +0000

    Adds search engine functions to crawler

--- crawler.py
+++ crawler.py
@@ -1,9 +1,22 @@
+# ============
+# Dummy Web Crawler
+# ============
+
 import requests
 
-seed = "http://pudim.com.br/"
+# seed = "http://pudim.com.br/"
+# Dummy pages content
+def get_page_content(url) -> str:
+    if url == "https://desserts.com/index.html":
+        return 'Choose your favorite dessert! <a href="http://pudim.com.br"> <a href="https://gelly.com">'
+    elif url == "http://pudim.com.br":
+        return requests.get("http://pudim.com.br").text
+    elif url == "https://gelly.com":
+        return "<h1>Im a Gelly lovers page<\h1>"
+    else:
+        raise NotImplementedError(f"No valid return for url {url}")
+    # return requests.get(url).text
 
-def get_page_content(url):
-    return requests.get(url).text
 
 def get_next_target(page: str) -> tuple:
     start_link = page.find('href=')
@@ -39,6 +52,7 @@ def union(p: list, q: list) -> None:
             p.append(item)
     return p
 
+
 def crawl_web(seed):
     ''' Starts crawling pages from the seed using Depth-first Search'''
     to_crawl =  [seed]
@@ -46,12 +60,39 @@ def crawl_web(seed):
     while to_crawl:
         page = to_crawl.pop()
         if page not in crawled:
-            print(page)
-            union(to_crawl, get_all_links(get_page_content(page)))
-            crawled.append(page)
+            try:
+                content = get_page_content(page)
+                add_page_to_index(index, page, content)
+                union(to_crawl, get_all_links(content))
+                crawled.append(page)
+            except Exception as e:
+                print(f"Couldnt get {page}", e)
+
     return crawled
 
-# print(crawl(seed))
-crawl_web(seed)
 
-# print(get_all_links(get_page_content(seed)))
+def add_page_to_index(index: list, url: str, content: str) -> list:
+    words = content.split()
+    for word in words:
+        is_word_in_keywords = False
+        for entry in index:
+            if word == entry[0]:
+                entry[1].append(url)
+                is_word_in_keywords = True
+        if not is_word_in_keywords:
+            index.append([word, [url]])
+    return index
+
+
+def lookup(index:list,keyword:str) -> list:
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+
+if __name__ == "__main__":
+    index = []
+    crawl_web('https://desserts.com/index.html')
+    # print(index)
+    print(lookup(index, 'lovers'))
