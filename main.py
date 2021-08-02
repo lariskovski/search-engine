537c475f84f4ef045db064e8c484a70321a37e7a
lariskovski <larissaporto@live.com>
2021-08-02 04:49:44 +0000

    Adds lookup_best to searcher module

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import logging
 from searchengine.crawler import crawl_web
-from searchengine.searcher import lookup
+from searchengine.searcher import lookup, lookup_best
 from searchengine.ranker import compute_ranks
 
 def main():
-    # logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.ERROR)
     
     seed = "https://udacity.github.io/cs101x/urank/" # Couple links example
     # seed = "https://gutenberg.org/cache/epub/1661/pg1661.txt" # A lot of keywords example
@@ -12,11 +12,18 @@ def main():
     index, graph = crawl_web(seed)
 
     ranks = compute_ranks(graph)
+    # prints unordered url ranking
     print(ranks)
 
-    print(lookup(index, 'buttercream'))
-    print(lookup(index, 'hummus'))
-    print(lookup(index, 'a'))
+    # Get all url for keyword
+    # print(lookup(index, 'buttercream'))
+    # print(lookup(index, 'the'))
+    # print(lookup(index, 'a'))
+
+    # Get best ranked url for keyword
+    print(lookup_best(index, 'buttercream', ranks))
+    print(lookup_best(index, 'the', ranks))
+    print(lookup_best(index, 'a', ranks))
 
 if __name__ == "__main__":
     main()
--- searchengine/searcher.py
+++ searchengine/searcher.py
@@ -1,5 +1,22 @@
 
-def lookup(index:list, keyword:str) -> None:
+def lookup(index:list, keyword:str) -> list:
     if keyword in index:
         return index[keyword]
+    return None
+
+def lookup_best(index: dict, keyword: str, ranks: dict) -> str:
+    urls_for_kw: list = lookup(index, keyword)
+
+    if urls_for_kw:
+        # Get set intersection between all possible urls for a given keyword and the ranked dictionary
+        url_rank_intersection: set = set(urls_for_kw).intersection(set(ranks.keys()))
+        # transforms set into a dict for later sorting
+        selected_ranks: dict = {key : ranks[key] for key in url_rank_intersection}
+        # sorts values in reverse order. gets most popular page
+        sort_selected_rank:list = [k for k, v in sorted(selected_ranks.items(), key=lambda item: item[1], reverse=True)]
+
+        best_url = sort_selected_rank[0]
+        
+        return best_url
+    
     return None
