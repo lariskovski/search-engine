971c699f064e0ce873e1e6e3d43fd65ffd026d99
lariskovski <larissaporto@live.com>
2021-08-03 08:48:34 +0000

    Fix index queue consumer

--- searchengine/index_queue_consumer.py
+++ searchengine/index_queue_consumer.py
@@ -4,16 +4,19 @@ import json
 import logging
 from dotenv import load_dotenv, find_dotenv
 
+# loggin profile
+# logging.basicConfig(level=logging.INFO)
+
 # RabbitMQ config
 load_dotenv(find_dotenv())
 INDEXER_API_URL =  os.getenv('INDEXER_API_URL')
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
 RABBITMQ_USER = os.getenv('RABBITMQ_USER')
 RABBITMQ_PASS = os.getenv('RABBITMQ_PASS')
+RABBITMQ_INDEX_QUEUE = os.getenv('RABBITMQ_INDEX_QUEUE')
 
 
 def main():
-    QUEUE = "add_to_index"
     # Setup
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
     parameters = pika.ConnectionParameters(RABBITMQ_HOST,
@@ -24,7 +27,7 @@ def main():
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    channel.queue_declare(queue=QUEUE)
+    channel.queue_declare(queue=RABBITMQ_INDEX_QUEUE)
 
     def callback(ch, method, properties, body):
         # data to be sent to api
@@ -33,14 +36,11 @@ def main():
                 'content': content}
         # sending post request and saving response as response object
         response = requests.post(url=INDEXER_API_URL, data=data)
-        # extracting response text 
-        response_text = response.text
-        response_code = response.status_code
-        print(response_text)
-        logging.info(f"index consumer recieved: {page} {content}")
+        print(response.text)
+        # logging.info(f"index consumer recieved: {page} {content}")
 
-    channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
-
+    # Consuming config
+    channel.basic_consume(queue=RABBITMQ_INDEX_QUEUE, on_message_callback=callback, auto_ack=True)
     print('Waiting for messages')
     channel.start_consuming()
 
