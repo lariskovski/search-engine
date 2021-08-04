f5db84e0d9307fd5d61cd4664d0bf16a8824720b
lariskovski <larissaporto@live.com>
2021-08-04 06:23:23 +0000

    Improves indexer queue consumer readability

--- searchengine/index_queue_consumer.py
+++ searchengine/index_queue_consumer.py
@@ -1,48 +1,62 @@
+from dotenv import load_dotenv, find_dotenv
 import pika, sys, os
 import requests
-import json
 import logging
-from dotenv import load_dotenv, find_dotenv
+import json
 
-# loggin profile
-# logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.INFO)
 
 # RabbitMQ config
 load_dotenv(find_dotenv())
-INDEXER_API_URL =  os.getenv('INDEXER_API_URL')
-RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
-RABBITMQ_USER = os.getenv('RABBITMQ_USER')
-RABBITMQ_PASS = os.getenv('RABBITMQ_PASS')
 RABBITMQ_INDEX_QUEUE = os.getenv('RABBITMQ_INDEX_QUEUE')
+INDEXER_API_URL      = os.getenv('INDEXER_API_URL')
+RABBITMQ_HOST        = os.getenv('RABBITMQ_HOST')
+RABBITMQ_USER        = os.getenv('RABBITMQ_USER')
+RABBITMQ_PASS        = os.getenv('RABBITMQ_PASS')
+
+
+def callback(ch, method, properties, body):
+    '''Defines how messages are gonna be treated as they are being consumed.
+       Fetches page and content values from body.
+       Sends them via POST request to Indexer API.
+    '''
+    # Fetches data to be sent to API
+    page, content = json.loads(body)
+
+    data = {
+             'page': page,
+             'content': content
+           }
+
+    # Sends POST request to Indexer API
+    response = requests.post(url=INDEXER_API_URL,
+                             data=data)
+    # Logs Indexer API response
+    logging.info(response.text)
 
 
 def main():
-    # Setup
+    # RabbitMQ setup
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
-    parameters = pika.ConnectionParameters(RABBITMQ_HOST,
-                                    5672,
-                                    '/',
-                                    credentials)
+    parameters  = pika.ConnectionParameters(RABBITMQ_HOST,
+                                           5672,
+                                           '/',
+                                           credentials)
 
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
+    connection  = pika.BlockingConnection(parameters)
+    channel     = connection.channel()
 
     channel.queue_declare(queue=RABBITMQ_INDEX_QUEUE)
 
-    def callback(ch, method, properties, body):
-        # data to be sent to api
-        page, content = json.loads(body)
-        data = {'page': page,
-                'content': content}
-        # sending post request and saving response as response object
-        response = requests.post(url=INDEXER_API_URL, data=data)
-        print(response.text)
-        # logging.info(f"index consumer recieved: {page} {content}")
+    # Queue consuming config
+    channel.basic_consume(queue=RABBITMQ_INDEX_QUEUE,
+                          on_message_callback=callback,
+                          auto_ack=True)
 
-    # Consuming config
-    channel.basic_consume(queue=RABBITMQ_INDEX_QUEUE, on_message_callback=callback, auto_ack=True)
-    print('Waiting for messages')
+    logging.info('Waiting for messages')
     channel.start_consuming()
+
 
 if __name__ == "__main__":
     main()
