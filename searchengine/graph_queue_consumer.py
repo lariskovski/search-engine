c12556d4a97b03b9dd1e2e551970c804ff703874
lariskovski <larissaporto@live.com>
2021-08-04 06:45:06 +0000

    Improves graph queue consumer redability

--- searchengine/graph_queue_consumer.py
+++ searchengine/graph_queue_consumer.py
@@ -7,30 +7,28 @@ import os
 
 
 load_dotenv(find_dotenv())
-INDEXER_API_URL =  os.getenv('INDEXER_API_URL')
 RABBITMQ_GRAPH_QUEUE = os.getenv('RABBITMQ_GRAPH_QUEUE')
-RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
-RABBITMQ_USER = os.getenv('RABBITMQ_USER')
-RABBITMQ_PASS = os.getenv('RABBITMQ_PASS')
+RABBITMQ_HOST        = os.getenv('RABBITMQ_HOST')
+RABBITMQ_USER        = os.getenv('RABBITMQ_USER')
+RABBITMQ_PASS        = os.getenv('RABBITMQ_PASS')
 
 
 def main():
     # Setup
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
-    parameters = pika.ConnectionParameters(RABBITMQ_HOST,
-                                    5672,
-                                    '/',
-                                    credentials)
+    parameters  = pika.ConnectionParameters(RABBITMQ_HOST,
+                                            5672,
+                                            '/',
+                                            credentials)
 
     connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
+    channel    = connection.channel()
 
     channel.queue_declare(queue=RABBITMQ_GRAPH_QUEUE)
 
     def callback(ch, method, properties, body):
-        # data to be sent to api
         message = json.loads(body)
-        # send infos to redis
+        # send infos to graph mongodb
         print(message)
 
     channel.basic_consume(queue=RABBITMQ_GRAPH_QUEUE, on_message_callback=callback, auto_ack=True)
