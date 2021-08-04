f76a7fba3c0ecc543582362e74c080f973bcd4c3
lariskovski <larissaporto@live.com>
2021-08-04 07:31:51 +0000

    Add mongodb to graph queue consumer

--- searchengine/graph_queue_consumer.py
+++ searchengine/graph_queue_consumer.py
@@ -1,40 +1,74 @@
 from dotenv import load_dotenv, find_dotenv
 import pika, sys, os
+import pymongo
 import requests
 import logging
 import json
-import os
 
 
+logging.basicConfig(level=logging.INFO)
+
+# RabbitMQ env
 load_dotenv(find_dotenv())
 RABBITMQ_GRAPH_QUEUE = os.getenv('RABBITMQ_GRAPH_QUEUE')
 RABBITMQ_HOST        = os.getenv('RABBITMQ_HOST')
 RABBITMQ_USER        = os.getenv('RABBITMQ_USER')
 RABBITMQ_PASS        = os.getenv('RABBITMQ_PASS')
 
+# MongoDB env
+MONGODB_COLLECTION = os.getenv('MONGODB_COLLECTION')
+MONGODB_CLUSTER    = os.getenv('MONGODB_CLUSTER')
+MONGODB_DB_NAME    = os.getenv('MONGODB_DB_NAME')
+MONGODB_USER       = os.getenv('MONGODB_USER')
+MONGODB_PASS       = os.getenv('MONGODB_PASS')
+
+# MongoDB configs
+try:
+    client     = pymongo.MongoClient(f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_CLUSTER}/")
+    db         = client[MONGODB_DB_NAME]
+    collection = db['graph']
+except ServerSelectionTimeoutError as e:
+    stderr.write(f"Could not connect to MongoDB: {e}")
+
+
+def callback(ch, method, properties, body):
+    '''Defines how messages are gonna be treated as they are being consumed.
+       Fetches page and url values from body.
+       Sends them to MongoDB graph collection.
+    '''
+    # Fetches data to be sent to API
+    page, urls = json.loads(body)
+    data = {
+             'page': page,
+             'urls' : [url for url in urls]
+           }
+    # Creates MongoDB document
+    result = collection.insert_one(data)
+    # Logs in format: {'page': 'x', 'urls': ['y], '_id': ObjectId('1234')}
+    logging.info(data)
+
 
 def main():
-    # Setup
+    # RabbitMQ setup
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
     parameters  = pika.ConnectionParameters(RABBITMQ_HOST,
                                             5672,
                                             '/',
                                             credentials)
 
-    connection = pika.BlockingConnection(parameters)
-    channel    = connection.channel()
+    connection  = pika.BlockingConnection(parameters)
+    channel     = connection.channel()
 
     channel.queue_declare(queue=RABBITMQ_GRAPH_QUEUE)
 
-    def callback(ch, method, properties, body):
-        message = json.loads(body)
-        # send infos to graph mongodb
-        print(message)
+    # Queue consuming config
+    channel.basic_consume(queue=RABBITMQ_GRAPH_QUEUE,
+                          on_message_callback=callback,
+                          auto_ack=True)
 
-    channel.basic_consume(queue=RABBITMQ_GRAPH_QUEUE, on_message_callback=callback, auto_ack=True)
-
-    print('Waiting for messages')
+    logging.info('Waiting for messages')
     channel.start_consuming()
+
 
 if __name__ == "__main__":
     main()
