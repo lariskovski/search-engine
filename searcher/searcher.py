8ce498c23ef2868af7985963d5b734402e09f564
lariskovski <larissaporto@live.com>
2021-08-05 07:17:27 +0000

    Adds redis on seachers lookup best

--- searcher/searcher.py
+++ searcher/searcher.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
+import requests, json
+import os, time
 import logging
 import pymongo
-import os
 
 # MongoDB envs
 load_dotenv(find_dotenv())
@@ -10,6 +11,8 @@ MONGODB_CLUSTER    = os.getenv('MONGODB_CLUSTER')
 MONGODB_DB_NAME    = os.getenv('MONGODB_DB_NAME')
 MONGODB_USER       = os.getenv('MONGODB_USER')
 MONGODB_PASS       = os.getenv('MONGODB_PASS')
+
+RANK_API           =os.getenv('RANK_API')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -34,25 +37,38 @@ def lookup(keyword:str) -> dict:
         return response['urls']
 
 
-def lookup_best(index: dict, keyword: str, ranks: dict) -> str:
-    urls_for_kw: list = lookup(index, keyword)
+def lookup_best(keyword: str) -> str:
+    ''' All possible urls for given keyword from lookup [url1, url2, url3]'''
+    urls_for_kw: list = lookup(keyword)
 
-    if urls_for_kw:
-        # Get set intersection between all possible urls for a given keyword and the ranked dictionary
-        url_rank_intersection: set = set(urls_for_kw).intersection(set(ranks.keys()))
-        # transforms set into a dict for later sorting
-        selected_ranks: dict = {key : ranks[key] for key in url_rank_intersection}
-        # sorts values in reverse order. gets most popular page
-        sort_selected_rank:list = [k for k, v in sorted(selected_ranks.items(), key=lambda item: item[1], reverse=True)]
+    unsorted_ranked_urls = {}
 
-        best_url = sort_selected_rank[0]
-        
-        return best_url
+    # Get each url's rank from Rank API
+    for url in urls_for_kw:
+        # Connection to Rank API. Retries if any exception is thrown
+        retry_api_connection = True
+        while retry_api_connection:
+            try:
+                request = requests.get(RANK_API + "/rank", params={ 'page': url })
+                response  = json.loads(request.text)
+                logging.debug(f"Successfully connected to {RANK_API}")
+                retry_api_connection = False
+
+            except Exception as e:
+                logging.critical(f"Could not connect to {RANK_API} - {e}")
+                logging.info(f"Retrying connection to {RANK_API}")
+                time.sleep(1)
+
+        # Adds entry {url: rank} to unsorted dict
+        url, rank = response['page'], response['rank']
+        unsorted_ranked_urls[url] = rank
+        logging.debug(f"Found rank for {url}: {rank}")
     
-    return None
+    # Sorts and returns best ranked page for the provided keyword
+    sorted_ranked_urls = {k: v for k, v in sorted(unsorted_ranked_urls.items(), key=lambda item: item[1])}
+    best_ranked        = sorted_ranked_urls.popitem()
+
+    return best_ranked
 
 if __name__ == "__main__":
-    s1 = lookup("hummus")
-    s2 = lookup("notfound")
-    # print(s1)
-    # print(s2)
+    print(lookup_best("hummus"))
